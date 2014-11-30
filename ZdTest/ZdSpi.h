40ddb14baf43a57cb45f519344c929a0d2c69309
aqls520 <aiqilingsha@gmail.com>
2014-12-01 01:25:41 +0800

    修改命名

--- ZdTest/ZdSpi.h
+++ ZdTest/ZdSpi.h
@@ -3,35 +3,39 @@
 #include "Lib_dll/ShZdMessage.h"
 #include "Lib_dll/ShZdTradeLib.h"
 
-#include "MyDefine.h"
-#include "MyStruct.h"
+#include "ZdDefine.h"
+#include "ZdStruct.h"
 
 class ZdSpi:public IShZdTradeOutLib
 {
 public:
+	//原始API响应
 	virtual int OnReceiveTradeInfo(const CShZdMessage * re);
 	virtual int OnReceiveMarketInfo(const CShZdMessage * re);
 	virtual int OnReceiveErrorInfo(int errorCode, const char* re);
 
+	//消息生成器
 	CShZdMessage* GetZdReqMessage(string MsgType, void* Msg);
+	//函数适配器
 	void ChgRspFunction(CShZdMessage* msg);
+	//Spi初始化
 	void Init();
 
-	//请求
+	//所有请求
 public:
 	void ReqLogin(LoginReq lr);//登录
 	void ReqAccountSearch(AccountSearchReq acr);//账户查询
 	void ReqExchangeSearch(ExchangeSearchReq esr);//交易所查询
 	void ReqOrder(OrderReq or);//报单
 	void ReqCancel(OrderCancelReq ocr);//撤单
-	void ReqModify(ModifyReq mr);
+	void ReqModify(ModifyReq mr);//改单
 	void ReqGetUserControl(ControlReq cr);//查询可用合约
 	void ReqMarket(MarketReq mr);//订阅行情
 	void ReqFillSearch(FillSearchReq fsr);//查询成交
 	void ReqOrderSearch(OrderSearchReq osr);//查询报单
 	void ReqOpenDetail(OpenDetailReq odr);//查询持仓
 
-	//响应
+	//所有响应
 public:
 	//登录响应
 	void RtnLoginReq(LoginRtn* lr, bool bIsLast);
@@ -56,6 +60,7 @@ public:
 	//撤单响应
 	void RtnCancelReq(OrderCancelRtn* ocr,bool bIsLast);
 
+	//报单的相关响应
 	void RtnSystemNo(SystemNoRtn* snr);
 	void RtnOrderBack(OrderBackRtn* obr); //O1:报单返回
 	void RtnOrderStatus(OrderStatusRtn* osr); //OST:报单状态返回
@@ -63,6 +68,7 @@ public:
 	void RtnOrderSysNo(); //SY：报单返回系统号
 	void RtnOrderFilled(OrderFilledRtn* ofr);//O3：报单成交返回
 
+	//改单响应
 	void RtnModifyReq(ModifyRtn* mr);
 
 
